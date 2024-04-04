5a818ac8f01095dc8fb047e64bc8dc5ea528c306
Sanduni-Abeysinghe <sanduniabeysinghe97@gmail.com>
2024-04-04 12:43:07 +0530

    test

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/inquiry/save/borwa', userController.saveUser);
+router.post('/inquiry/save', userController.saveUser);
 
 module.exports = router;
